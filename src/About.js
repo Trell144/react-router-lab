5020fbaf3da7dcc825ea0ee27e4fad0addd51bae
Catrell Matthews <catrellmatthewss@gmail.com>
2018-10-03 18:20:19 -0400

    homework

--- src/About.js
+++ src/About.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
-class About extends Component {
-  render () {
+export default class Home extends Component {
+
+  render() {
+    let stocks = this.props.stocks.map((stock, index) => {
+      let pathname = `/stocks/${stock.symbol}`
+      return 
+        <li key={index}>
+          {stock.name} <Link to={pathname}>Go to Stock Page</Link>
+        </li>
+        console.log('stocks',stocks)
+    })
     return (
       <div>
-        <h2>This is the about page.</h2>
+      <h1>Stocks</h1>
+      <div>
+        <ul>
+        {stocks}
+        </ul>
+      </div>
       </div>
     )
   }
 }
-
-export default About
---
+++ src/components/stocks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class stock extends Component {
+
+const stock = this.props.stocks.find (stock) => {
+
+}
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
